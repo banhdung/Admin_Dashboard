839f2029092f3df6e475b66983f3e84aa63545cf
banhdung <lmpdungbd@gmail.com>
2023-11-01 19:03:09 +0700

    completed css

---
+++ PRN221_Project/PRN221_Project/Pages/Admin/Customers/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PRN221_Project.Models;
+
+namespace PRN221_Project.Pages.Customers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly PRN221_Project.Models.POSTContext _context;
+
+        public CreateModel(PRN221_Project.Models.POSTContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Customers == null || Customer == null)
+            {
+                return Page();
+            }
+
+            _context.Customers.Add(Customer);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ PRN221_Project/PRN221_Project/Pages/Admin/Customers/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PRN221_Project.Models;
+
+namespace PRN221_Project.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PRN221_Project.Models.POSTContext _context;
+
+        public IndexModel(PRN221_Project.Models.POSTContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customer> Customer { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Customers != null)
+            {
+                Customer = await _context.Customers.ToListAsync();
+            }
+        }
+    }
+}
